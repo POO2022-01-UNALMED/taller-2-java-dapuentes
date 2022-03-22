bcef4a6a50aa9f90567cbe18a6602d9c5e172c8c
dapuentes <dapuentes@unal.edu.co>
2022-03-22 16:40:10 -0500

    Asiento

---
+++ src/main/java/Auto.java
@@ -0,0 +1,17 @@
+package test;
+public class Auto {
+	String modelo;
+	int precio;
+	String marca;
+	Asiento[] asientos;
+	Motor motor;
+	int registro;
+	static int cantidadCreados;
+	
+	public void int cantidadAsientos() {
+		
+		
+	}
+	
+
+}
